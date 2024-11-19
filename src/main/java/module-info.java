6a82892d126509d2d9f33c66d75fb25a31842bc0
FadilahSyed <fadilahsyedazlan@gmail.com>
2024-11-19 19:52:48 +0800

    fixed package org error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,10 @@ module com.example.demo {
     requires javafx.fxml;
 
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo.controller;
+    opens com.example.demo.controller to javafx.fxml;
     opens com.example.demo.ui to javafx.fxml;
     opens com.example.demo.core to javafx.fxml;
     opens com.example.demo.actors to javafx.fxml;
+
+    exports com.example.demo.controller;
 }
