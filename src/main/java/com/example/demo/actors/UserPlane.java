16790b5e53afa1569c826ff206724623bd6775dd
FadilahSyed <fadilahsyedazlan@gmail.com>
2024-11-27 16:45:14 +0800

    horizontal movement for userplane

--- src/main/java/com/example/demo/actors/UserPlane.java
+++ src/main/java/com/example/demo/actors/UserPlane.java
@@ -11,32 +11,54 @@ public class UserPlane extends FighterPlane {
 	private static final double Y_LOWER_BOUND = 600.0;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
+	private static final double X_LEFT_BOUND=5.0;
+	private static final double X_RIGHT_BOUND=1200.0;
+
 	private static final int IMAGE_HEIGHT = 42;
-	private static final int VERTICAL_VELOCITY = 8;
+	private static final int VELOCITY = 8;
 	private static final int PROJECTILE_X_POSITION = 110;
 	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
 
 	//instance variables
-	private int velocityMultiplier;
+	private int verticalVelocityMultiplier;
+	private int horizontalVelocityMultiplier;
 	private int numberOfKills;
 
 	//constructor
 	public UserPlane(int initialHealth) {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
-		velocityMultiplier = 0;
+		this.verticalVelocityMultiplier = 0;
+		this.horizontalVelocityMultiplier=0;
 	}
 
 	//public methods
 	@Override
 	public void updatePosition() {
-		if (isMoving()) {
+		double initialTranslateY=getTranslateY();
+		double initialTranslateX=getTranslateX();
+
+		if(verticalVelocityMultiplier!=0) {
+			this.moveVertically(VELOCITY*verticalVelocityMultiplier);
+			double newYPosition=getLayoutY()+getTranslateY();
+			if(newYPosition<Y_UPPER_BOUND || newYPosition>Y_LOWER_BOUND) {
+				this.setTranslateY(initialTranslateY);
+			}
+		}
+		if(horizontalVelocityMultiplier!=0) {
+			this.moveHorizontally(VELOCITY*horizontalVelocityMultiplier);
+			double newXPosition=getLayoutX()+getTranslateX();
+			if(newXPosition<X_LEFT_BOUND || newXPosition>X_RIGHT_BOUND) {
+				this.setTranslateY(initialTranslateX);
+			}
+		}
+		/*if (isMoving()) {
 			double initialTranslateY = getTranslateY();
 			this.moveVertically(VERTICAL_VELOCITY * velocityMultiplier);
 			double newPosition = getLayoutY() + getTranslateY();
 			if (newPosition < Y_UPPER_BOUND || newPosition > Y_LOWER_BOUND) {
 				this.setTranslateY(initialTranslateY);
 			}
-		}
+		}*/
 	}
 	
 	@Override
@@ -50,15 +72,25 @@ public class UserPlane extends FighterPlane {
 	}
 
 	public void moveUp() {
-		velocityMultiplier = -1;
+		verticalVelocityMultiplier = -1;
 	}
 
 	public void moveDown() {
-		velocityMultiplier = 1;
+		verticalVelocityMultiplier = 1;
+	}
+	public void moveLeft() {
+		horizontalVelocityMultiplier = -1;
 	}
 
-	public void stop() {
-		velocityMultiplier = 0;
+	public void moveRight() {
+		horizontalVelocityMultiplier = 1;
+	}
+
+	public void stopVertical() {
+		verticalVelocityMultiplier = 0;
+	}
+	public void stopHorizontal(){
+		horizontalVelocityMultiplier=0;
 	}
 
 	public int getNumberOfKills() {
@@ -70,8 +102,8 @@ public class UserPlane extends FighterPlane {
 	}
 
 	//private methods
-	private boolean isMoving() {
+	/*private boolean isMoving() {
 		return velocityMultiplier != 0;
-	}
+	}*/
 
 }
