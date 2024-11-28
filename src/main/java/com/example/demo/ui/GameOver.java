8442402f58400f5af8f56d93563d6028c20481f3
FadilahSyed <fadilahsyedazlan@gmail.com>
2024-11-28 21:24:26 +0800

    formatted MainMenu + GameOverScreen (background,label,buttons)

--- src/main/java/com/example/demo/ui/GameOver.java
+++ src/main/java/com/example/demo/ui/GameOver.java
@@ -8,38 +8,56 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GameOver {
-    private static final String GAMEOVER_BG = "gameover.jpeg";
-    private static final String START_IMAGE = "startbutton.png";
-    private static final String EXIT_IMAGE = "exitbutton.png";
+    private static final String BACKGROUND_IMAGE_NAME = "gameOverBackground.jpeg";
+    private static final String GAMEOVER_IMAGE = "gameOverLabelImage.png";
+    private static final String REPLAY_IMAGE = "replayButtonImage.png";
+    private static final String EXIT_IMAGE = "exitButtonImage.png";
+    private static final int BUTTON_HEIGHT=75;
+    private static final int BUTTON_WIDTH=250;
+    private static final int TITLE_HEIGHT=150;
+    private static final int TITLE_WIDTH=600;
+    private static final int SCREEN_WIDTH = 1300;
+    private static final int SCREEN_HEIGHT = 750;
     private final Stage stage;
     private final Runnable onReplay;
-    //private final ImageView background;
+    private final ImageView background;
 
     public GameOver(Stage stage, Runnable onReplay) {
         this.stage=stage;
         this.onReplay=onReplay;
+        this.background = new ImageView(ImageLoader.load(BACKGROUND_IMAGE_NAME));
     }
 
     public void show() {
+        StackPane root = new StackPane();
         //ImageView background = new ImageView(ImageLoader.load(GAMEOVER_BG));
+        background.setFitHeight(SCREEN_HEIGHT);
+        background.setFitWidth(SCREEN_WIDTH);
+        root.getChildren().add(background);
+
         VBox layout = new VBox(20);
         layout.setAlignment(Pos.CENTER);
         //layout.setStyle("-fx-padding: 30; -fx-alignment: center; -fx-background-image: ;");
 
+        ImageView gameOverLabel = new ImageView(ImageLoader.load(GAMEOVER_IMAGE));
+        gameOverLabel.setFitWidth(TITLE_WIDTH);
+        gameOverLabel.setFitHeight(TITLE_HEIGHT);
         // Buttons
-        Button replayButton=ImageButton(START_IMAGE,200,50);
-        Button exitButton=ImageButton(EXIT_IMAGE,200,50);
+        Button replayButton=ImageButton(REPLAY_IMAGE,BUTTON_WIDTH,BUTTON_HEIGHT);
+        Button exitButton=ImageButton(EXIT_IMAGE,BUTTON_WIDTH,BUTTON_HEIGHT);
 
         replayButton.setOnAction(event-> onReplay.run());
         exitButton.setOnAction(event->stage.close());
 
-        layout.getChildren().addAll(replayButton,exitButton);
+        layout.getChildren().addAll(gameOverLabel,replayButton,exitButton);
 
-        Scene scene =new Scene(layout,600,400);
+        root.getChildren().add(layout);
+        Scene scene =new Scene(root,600,400);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/ui/MainMenu.java
+++ src/main/java/com/example/demo/ui/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.demo.ui;
 import com.example.demo.controller.Controller;
 import com.example.demo.utils.ImageLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
@@ -12,8 +13,14 @@ import javafx.geometry.Pos;
 
 public class MainMenu {
     private static final String BACKGROUND_IMAGE_NAME = "gameoverbg.jpeg";
-    private static final String START_IMAGE = "startbutton.png";
-    private static final String EXIT_IMAGE = "exitbutton.png";
+    private static final String TITLE_IMAGE = "titleLabelImage.png";
+    private static final String START_IMAGE = "startButtonImage.png";
+    private static final String TUTORIAL_IMAGE = "tutorialButtonImage.png";
+    private static final String EXIT_IMAGE = "exitButtonImage.png";
+    private static final int BUTTON_HEIGHT=75;
+    private static final int BUTTON_WIDTH=250;
+    private static final int TITLE_HEIGHT=150;
+    private static final int TITLE_WIDTH=600;
     private static final int SCREEN_WIDTH = 1300;
     private static final int SCREEN_HEIGHT = 750;
 
@@ -30,8 +37,8 @@ public class MainMenu {
 
     public void show() {
         StackPane root = new StackPane();
-        background.requestFocus();
-        background.setFocusTraversable(true);
+        //background.requestFocus();
+        //background.setFocusTraversable(true);
         background.setFitHeight(SCREEN_HEIGHT);
         background.setFitWidth(SCREEN_WIDTH);
         root.getChildren().add(background);
@@ -51,9 +58,15 @@ public class MainMenu {
         VBox layout = new VBox(20); // Vertical box with 10px spacing
         layout.setAlignment(Pos.CENTER);
 
-        Button startGameButton=ImageButton(START_IMAGE,200,50);
-        Button tutorialButton=ImageButton(START_IMAGE,200,50);
-        Button exitButton=ImageButton(EXIT_IMAGE,200,50);
+        ImageView gameTitleLabel = new ImageView(ImageLoader.load(TITLE_IMAGE));
+        gameTitleLabel.setFitWidth(TITLE_WIDTH);
+        gameTitleLabel.setFitHeight(TITLE_HEIGHT);
+        //Label gameTitleLabel = new Label("SKY BATTLE!");
+        //gameTitleLabel.setStyle("-fx-font-size: 75px; -fx-text-fill: black;");
+
+        Button startGameButton=ImageButton(START_IMAGE,BUTTON_WIDTH,BUTTON_HEIGHT);
+        Button tutorialButton=ImageButton(TUTORIAL_IMAGE,BUTTON_WIDTH,BUTTON_HEIGHT);
+        Button exitButton=ImageButton(EXIT_IMAGE,BUTTON_WIDTH,BUTTON_HEIGHT);
 
 
         // Create Buttons
@@ -69,15 +82,16 @@ public class MainMenu {
         exitButton.setOnAction(e -> stage.close());
 
         // Add buttons to layout
-        layout.getChildren().addAll(startGameButton, tutorialButton, exitButton);
+        layout.getChildren().addAll(gameTitleLabel,startGameButton, tutorialButton, exitButton);
 
+        root.getChildren().add(layout);
         // Set the scene and display the stage
-        Scene scene = new Scene(layout, 600, 400);
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
 
-    private Button ImageButton(String imagePath, int width, int height) {
+    private Button ImageButton(String imagePath,int width, int height) {
         ImageView imageView= new ImageView(ImageLoader.load(imagePath));
         imageView.setFitWidth(width);
         imageView.setFitHeight(height);
